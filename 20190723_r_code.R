78a8b7611a92278f49daab4fa0250f4f4327042b
Asta Lee <astalee12@gmail.com>
2019-07-23 21:14:16 +0800

    Update 20190723_r_code.R

--- 20190723_r_code.R
+++ 20190723_r_code.R
@@ -127,6 +127,216 @@ t.stat
 t.test(iris[,1],iris[,2],var.equal = T)
 
 
+##函數內的變數##  #雙箭頭的意思#
+myfun1 <- function(x){   
+  y <- x + 5
+  cat("y: ", y, "\n")
+}
+
+#使用雙箭頭#
+myfun2 <- function(x){   
+  y <<- x + 5
+  cat("y: ", y, "\n")
+}
+
+y <- 5; cat("y: ", y, "\n")
+myfun1(3)
+cat("y: ", y, "\n")
+
+y <- 5; cat("y: ", y, "\n")
+myfun2(3)
+cat("y: ", y, "\n")
+
+myfun1 <- function(x){   
+  y <- x + 5
+  cat("y: ", y, "\n")
+}
+
+myfun2 <- function(x){   
+  y <<- x + 5
+  cat("y: ", y, "\n")
+}
+
+y <- 5; cat("y: ", y, "\n")
+myfun1(3)
+cat("y: ", y, "\n")
+
+
+y <- 5; cat("y: ", y, "\n")
+myfun2(3)
+cat("y: ", y, "\n")
+
+## for 迴圈##
+for(i in 1:5){
+  cat("loop: ", i, "\n")
+}
+
+for(k in c(1, 17, 3, 56, 2)){
+  cat(k, "\t")
+}
+
+for(bloodType in c("A", "AB", "B", "O")){
+  cat(bloodType, "\t")
+}
+
+
+rm(list=ls())
+y <- round(rnorm(10), 2)
+z <- y
+y
+i
+
+for(i in 1:length(y)){
+  if(y[i] < 0) 
+    y[i] <-0
+}
+y
+i
+
+z[z<0] <- 0
+z
 
 
 
+## for 雙迴圈 ##  #竟量避免使用for 雙迴圈#
+a <- numeric(5)
+for(i in 1:5){
+  a[i]<- i^2
+}
+a
+
+
+m <- 3
+n <- 4
+for(i in 1:m){
+  for(j in 1:n){
+    cat("loop: (", i, ",", j, ")\n")
+  }
+}
+
+
+a <- matrix(0,2,4)
+for(i in 1:2){
+  for(j in 1:4){ 
+    a[i,j]<- i+j
+  }
+}
+a
+
+
+## 迴圈控制: next，省略此次迴圈，直接執行下一個迴圈##
+
+m <- 3
+n <- 4
+for(i in 1:m){
+  for(j in 1:n){
+    
+    if(i==2){
+      cat("before next:", i,",",j, "\n")
+      next
+      cat("after next:", i,",",j, "\n")
+    }else{
+      cat("loop: (", i, ",", j, ")\n")
+    }
+  }
+}
+
+
+
+## 迴圈控制: break 跳出當下的迴圈##
+m <- 3
+n <- 4
+for(i in 1:m){
+  for(j in 1:n){
+    
+    if(i==2){
+      cat("before break:", i,",",j, "\n")
+      break
+      cat("after break:", i,",",j, "\n")
+    }else{
+      cat("loop: (", i, ",", j, ")\n")
+    }
+  }
+}
+
+
+
+
+##課堂練習: 判斷一正整數是否為質數##
+
+check.prime <- function(num){
+  
+  yes <- FALSE
+  
+  if(num == 2){
+    yes <- TRUE
+    
+  } else if(num > 2) {
+    
+    yes <- TRUE
+    for(i in 2:(num-1)) {
+      if ((num %% i) == 0) {
+        yes <- FALSE
+        break
+      }
+    }
+  } 
+  
+  if(yes) {
+    cat(num, "is a prime number. \n")
+  } else {
+    cat(num, "is not a prime number. \n")
+  }
+}
+
+
+check.prime(2)
+check.prime(13)
+check.prime(25)
+
+
+## 判別條件 : repeat and while，用於我不知道要跑幾次的時候 ##
+#repeat沒有判別，所以至少會執行一次#
+#while會判別之後再執行，所以有可能不執行#
+
+
+a <- 5
+while(a > 0){  
+  a <- a-1  
+  cat(a,"\n")
+  if(a==2){
+    cat("before next:", a, "\n")
+    next
+    cat("after next:", a, "\n")
+  }
+}
+
+# 換成break #
+a <- 5
+while(a > 0){  
+  
+  if(a == 2){
+    cat("before break:", a, "\n")
+    break
+  }
+  a <- a-1  
+  cat(a,"\n")
+}
+
+# 無窮迴圈 : 按esc / Ctrl+c / Ctrl+z 停止計算# #這邊我不懂#
+a <- 5
+while(a > 0){  
+  
+  if(a==2){
+    cat("before break:", a, "\n")
+    next
+    cat("after break:", a, "\n")
+  }
+  a <- a-1  
+  cat(a,"\n")
+}
+
+##課堂練習10 : 計算n! 看看哪一個方式比較快##
+
+##額外練習: n! 使用向量式來計算，連乘##
+prod(1:10)
