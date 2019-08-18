d91ef8fa1a0f2b88dd1595f1e5f3bb35825b2f33
Asta Lee <astalee12@gmail.com>
2019-08-18 20:22:04 +0800

    Update 108-1-exam2.R

--- 108-1-exam2.R
+++ 108-1-exam2.R
@@ -8,6 +8,7 @@ LinearAlgebra <- sample(1:100, length(student.id), replace = T)
 aa <- data.frame(student.id,Calculus,LinearAlgebra)
 aa$note <- ifelse((aa$Calculus>=85 & aa$LinearAlgebra >= 85),"佳", ifelse((aa$Calculus<40 & aa$LinearAlgebra <40),"危險",ifelse((aa$Calculus<= 40 | aa$LinearAlgebra <= 40 ),"要加強","")))
 table(aa$note)
+aa[aa$note =="佳" | aa$note == "危險"]
 
 #1-2#
 x <- c(-5:5)
@@ -23,5 +24,13 @@ no <- sample(20:100, 1)
 nv <- LETTERS[sample(1:26, 5)][sample(1:5, no, replace=T)]
 
 
+iqv <- function(x){
+  n <- length(x)
+  k <- length(unique(x))
+  df <- as.data.frame(table(x))
+  f <- sum((df[,2])^2)
+  i <- k*((n^2)-f) / ((n^2)-(k-1))
+  i
+}
 
-
+iqv(nv)
