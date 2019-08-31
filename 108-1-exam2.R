388d9d0a6552825e5e71e7ba1578d4821890646a
Asta Lee <astalee12@gmail.com>
2019-08-31 14:31:36 +0800

    Update 108-1-exam2.R

--- 108-1-exam2.R
+++ 108-1-exam2.R
@@ -10,13 +10,59 @@ aa$note <- ifelse((aa$Calculus>=85 & aa$LinearAlgebra >= 85),"佳", ifelse((aa$C
 table(aa$note)
 aa[aa$note =="佳" | aa$note == "危險"]
 
-#1-2#
+#正確版#
+#1-a#
+set.seed(12345)
+student.id <- paste("student", 1:50, sep=".")
+Calculus <- round(rnorm(length(student.id),mean = 65, sd = 10),0)
+LinearAlgebra <- sample(1:100, length(student.id), replace = T)
+
+mydata <- data.frame(Calculus,LinearAlgebra)
+row.names(mydata) <- student.id
+mydata
+
+take_note <- function(x){
+  # x <- mydata # #for test#
+  n <- nrow(x)
+  note <- character(n)
+  for (i in 1:n){
+    if(x[i,1] > 85 & 
+       x[i ,2] > 85){
+      note[i] <- "good"
+    }
+    if(x[i,1] < 40 | 
+       x[i,2] < 40 ){
+      note[i] <- "fair"
+    }
+    if(x[i,1] < 40 & 
+       x[i,2] < 40 ){
+      note[i] <- "bad"
+    }
+  }
+  print(table(note))
+  cat("\n")
+  data.frame(x,note)
+}
+
+
+
+#2#
 x <- c(-5:5)
 y <-data.frame(x,ifelse(x<0,abs((x^2)+x),ifelse((x>=0 & x<3),sin(x),ifelse(x>=3,3*exp(x),""))))
 names(y) <- c("x","f(x)")
 y
 
 
+#2-correct#
+f <- function(x){
+  ifelse(x < 0, abs((x^2) + x),
+         ifelse((x >= 0 & x < 3), sin(x),
+                ifelse(x >= 3, 3*exp(x), "")))
+}
+
+x <- seq(-5,5,1)
+x
+f(x)
 
 #1-3#
 set.seed(12345)
